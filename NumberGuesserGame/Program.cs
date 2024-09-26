c1ef0c566db0004e0360a6053e4d471657e989ac
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-26 14:03:33 +0530

    Add files via upload

---
+++ NumberGuesserGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesserGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber, guessingNumber;
+
+            Console.WriteLine("Enter the secret number: ");
+            secretNumber =Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Range set by game master is 1-100");
+            Console.WriteLine("Guess the number: ");
+            guessingNumber = Convert.ToInt32(Console.ReadLine());
+
+            while (guessingNumber != secretNumber)
+            {
+                if (guessingNumber < secretNumber) Console.WriteLine("The number is too low, try again.");
+                else Console.WriteLine("The number is too high, try again.");
+
+                Console.WriteLine("Guess the number again!");
+                guessingNumber=Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Congrats! The number is right!");
+        }
+    }
+}
